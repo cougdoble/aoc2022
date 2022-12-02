7d2209c176fb761ad8fe8708025223c6d7e57d48
Doug <13511246+cougdoble@users.noreply.github.com>
2022-12-02 13:34:20 -0500

    get ready for day03

---
+++ AOC2022/Solutions/Day03/Solution.cs
@@ -0,0 +1,18 @@
+namespace AOC2022.Solutions.Day03;
+
+public class Solution : SolutionBase
+{
+    internal Solution() : base(03, 2022, "", true)
+    {
+    }
+
+    protected override string SolvePartOne()
+    {
+        return "";
+    }
+
+    protected override string SolvePartTwo()
+    {
+        return "";
+    }
+}
